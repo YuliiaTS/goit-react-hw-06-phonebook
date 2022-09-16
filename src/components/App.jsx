77fe38d2776a99ff533d9af47a02da14bb3b2764
Yuliia Kovalchuk <iuliia1702@gmail.com>
2022-09-17 00:00:36 +0300

    зробитиФорму

---
+++ src/components/App.jsx
@@ -0,0 +1,40 @@
+import { useState, useEffect } from 'react';
+import style from '../components/App.module.css';
+// import ContactForm from './ContactForm/ContactForm';
+import ContactList from './ContactList/ContactList';
+import Filter from './Filter/Filter';
+
+const contactsList = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
+
+export default function App() {
+  const [contacts, setContacts] = useState(
+    () => JSON.parse(window.localStorage.getItem('contacts')) ?? contactsList
+  );
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  // const onSubmit = newContact => {
+  //   if (contacts.find(contact => contact.name === newContact.name)) {
+  //     return alert(newContact.name + ' is alredy in contacts.');
+  //   } else {
+  //     setContacts([...contacts, newContact]);
+  //   }
+  // };
+
+  return (
+    <div className={style.main}>
+      <h1>Phonebook</h1>
+      {/* <ContactForm onSubmit={onSubmit} /> */}
+      <h2>Contacts</h2>
+      <Filter />
+      <ContactList />
+    </div>
+  );
+}
